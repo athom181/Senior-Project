c4a5a6232a85668872e43cdc6899d6f0e613d418
athom181 <54421344+athom181@users.noreply.github.com>
2020-12-24 18:29:58 -0500

    Add files via upload

--- src/pages/info.js
+++ src/pages/info.js
@@ -13,6 +13,9 @@ const Home = () => {
 				<div className="col-12 col-md-10 text-center p-3">
 					<h1>Water Utility Calculator App</h1>
 					<h4>A basic breakdown of how your bill is calculated. For more information visit <a target="_blank" href="https://charlottenc.gov/Water/RatesBilling/Pages/CLTWRates.aspx">Charlotte Water</a>.</h4>				
+					<h4>NOTE: This calculator will calculate bills for a 30 day period. The Fixed and Availability fees are billed based on how many days of service is on your bill.
+						This bill is set up to ONLY generate 30 day bills.
+					</h4>
 				</div>
 				<div>
 					<h3>
